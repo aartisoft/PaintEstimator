f9f315e86e52584a951414a7304537eee6741247
phuynh101 <huynhhuuphuc0607@gmail.com>
2017-09-19 17:13:34 -0700

    change lanucher icon, add backgound

--- app/src/main/java/edu/orangecoastcollege/cs273/phuynh101/paintestimator/HelpActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/phuynh101/paintestimator/HelpActivity.java
@@ -1,7 +1,9 @@
 package edu.orangecoastcollege.cs273.phuynh101.paintestimator;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.TextView;
 
 /**
@@ -23,5 +25,16 @@ public class HelpActivity extends AppCompatActivity {
         mGallonsTextView = (TextView) findViewById(R.id.gallonsTextView);
         mGallonsTextView.setText(getString(R.string.estimated_paint_required) +
                 String.valueOf(getIntent().getExtras().getFloat("gallons")) + " gallons");
+    }
+
+    /**
+     * Go back to the main activity
+     * @param v the view that triggers the event
+     */
+    protected void goBack(View v)
+    {
+        Intent goBackIntent = new Intent(this, MainActivity.class);
+        startActivity(goBackIntent);
+        finish();
     }
 }
